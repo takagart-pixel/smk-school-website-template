4270d077428665e852de45d7cb6c10fcc170ba37
Jeremiah Lengkong <jerlenh12@gmail.com>
2025-06-02 20:49:05 +0800

    Update - Menambahkan animasi kepada seluruh page menggunakan SAL.Js

--- assets/js/sal.js
+++ assets/js/sal.js
@@ -1,4 +1,4 @@
 sal({
-  threshold: 0.3, // Animasi muncul saat elemen 50% terlihat di layar
+  threshold: 0.1, // Animasi muncul saat elemen 10% terlihat di layar
   once: false, // Animasi hanya muncul sekali (opsional)
 });
