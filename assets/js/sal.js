b3887b4e6eb5e4351bec49b392652115fd653265
Jeremiah Lengkong <jerlenh12@gmail.com>
2025-06-15 09:19:17 +0800

    Update - Memperbarui page

--- assets/js/sal.js
+++ assets/js/sal.js
@@ -1,4 +1,4 @@
 sal({
   threshold: 0.1, // Animasi muncul saat elemen 10% terlihat di layar
-  once: false, // Animasi hanya muncul sekali (opsional)
+  once: true, // Animasi hanya muncul sekali (opsional)
 });
